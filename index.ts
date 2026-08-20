654dff4bb68646f2ab8f2ac616764f7666e762ba
AstroNRE <yurisergeev564@gmail.com>
2026-08-20 12:41:37 +0300

    fix

--- index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./api";
-export * from "./dishes";
-export * from "./flavor";
--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  transpilePackages: ["@tastecraft/contracts", "@tastecraft/flavor-engine"],
-  poweredByHeader: false
-};
-
-export default nextConfig;
